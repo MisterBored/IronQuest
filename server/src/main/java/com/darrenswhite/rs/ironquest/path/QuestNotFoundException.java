11a7e14cb9239b2ed116908300958a8211a42db6
MisterBored <isaiahpringle@gmail.com>
2025-08-04 07:25:12 -0400

    Annotate QuestNotFoundException with 404 and test

--- server/src/main/java/com/darrenswhite/rs/ironquest/path/QuestNotFoundException.java
+++ server/src/main/java/com/darrenswhite/rs/ironquest/path/QuestNotFoundException.java
@@ -2,6 +2,8 @@ package com.darrenswhite.rs.ironquest.path;
 
 import com.darrenswhite.rs.ironquest.path.algorithm.PathFinderAlgorithm;
 import com.darrenswhite.rs.ironquest.quest.Quest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception thrown when {@link PathFinderAlgorithm} fails to find the next optimal {@link Quest} to
@@ -9,6 +11,7 @@ import com.darrenswhite.rs.ironquest.quest.Quest;
  *
  * @author Darren S. White
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class QuestNotFoundException extends Exception {
 
   public QuestNotFoundException(String message) {
